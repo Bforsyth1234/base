ae6512dbd53c7ba617b5cbb39d1a072cb83ef6cf
Brooks Forsyth <brooks.forsyth@gmail.com>
2020-11-25 09:43:35 -0500

    added ngxs

--- apps/base-app/src/app/app.module.ts
+++ apps/base-app/src/app/app.module.ts
@@ -8,11 +8,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { NgxsModule } from '@ngxs/store';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, NgxsModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
